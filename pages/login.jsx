f9fd049255b6941350bec7fa5b87d39ceed08db5
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-02-23 17:35:24 +0700

    sudah bisa redirect

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
+import cookies from 'next-cookies'
+
 import styles from '../styles/Home.module.css'
 
-const login = () => {
+const login = ({ allCookies }) => {
     return (
         <div className={styles.container}>
             <p>Login Page</p>
         </div>
     )
+}
+
+export const getServerSideProps = async ctx => {
+    const allCookies = cookies(ctx)
+
+    if (!allCookies.token) {
+        return ctx.res.writeHead(302, { Location: '/' }).end()
+    }
+
+    return {
+        props: {
+            allCookies,
+        },
+    }
 }
 
 export default login
